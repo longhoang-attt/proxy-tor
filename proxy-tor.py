e71da412f8201eda2dae2d61d0faf3b0807492fe
longhoang <103498509+longhoang-attt@users.noreply.github.com>
2022-05-30 14:33:46 +0700

    Update proxy-tor.py

--- proxy-tor.py
+++ proxy-tor.py
@@ -8,9 +8,6 @@ import subprocess
 
 #####################-check-appps
 try:
-    check_pip3 = subprocess.check_output('dpkg -s python3-pip', shell=True)
-    if str('install ok installed') in str(check_pip3):
-        pass
 except subprocess.CalledProcessError:
     print('[+] pip3 not installed')
     subprocess.check_output('sudo apt install python3-pip -y', shell=True)
@@ -31,16 +28,6 @@ except subprocess.CalledProcessError:
     print('[+] tor is not installed !')
     subprocess.check_output('sudo apt install tor -y',shell=True)
     print('[!] tor is installed succesfully ')
-
-try:
-
-    check_firefox = subprocess.check_output('which firefox', shell=True)
-except subprocess.CalledProcessError:
-
-    print('[+] firefox is not installed !')
-    subprocess.check_output('sudo apt install firefox -y',shell=True)
-    print('[!] firefox is installed succesfully ')
-
 try:
 
     check_proxychains = subprocess.check_output('which proxychains', shell=True)
@@ -51,7 +38,7 @@ except subprocess.CalledProcessError:
     print('[!] proxychains  is installed succesfully ')
 
 def change():
-    os.system("sudo service tor start >> /dev/null && sudo service tor reload >> /dev/null ")
+os.system("sudo service tor start >> /dev/null && sudo service tor reload >> /dev/null && sudo proxychains curl icanhazip.com ")
 
 ##################################- start
 os.system("clear")
@@ -68,22 +55,19 @@ print('''\033[1;32;40m \n
                                [ FROM : longhoang.attt ] v 1.0
 
 ''')
-print("\033[1;40;31m contact : longhoang.attt@gmail.com \n")
-print("")
+print("\033[1;40;31m contact : longhoang.attt@gmail.com \n
+")
 print("\033[1;32;40m Tools to keep you anonymous on your website and access the deepweb more easily than ever before \n")
 time.sleep(3)
 x = input("[+] time to change Ip in Sec >_  ")
 lin = input("[+] how many time do you want to change your ip , for infinte ip change type >_ ")
 if int(lin) ==int(0):
-
     while True:
         try:
             time.sleep(int(x))
             change()
         except KeyboardInterrupt:
-
             quit()
-
 else:
     for i in range(int(lin)):
             time.sleep(int(x))
